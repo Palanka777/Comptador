02c1a198d6fd20562134d07f98fcfa505266c9bb
Palanka777 <dpont@iesebre.com>
2021-10-10 23:51:50 +0200

    Actualitzo la versio 0.2 de comptador

--- app/src/main/java/me/davidpont/dam/comptador/MainActivity.kt
+++ app/src/main/java/me/davidpont/dam/comptador/MainActivity.kt
@@ -2,10 +2,40 @@ package me.davidpont.dam.comptador
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    internal lateinit var tapMeButton : Button
+    internal lateinit var timeTextView : TextView
+    internal lateinit var counterTextView: TextView
+    internal var counter=0;
+    internal var time= 60;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tapMeButton = findViewById(R.id.tapMeButton)
+        timeTextView = findViewById(R.id.timeTextView)
+        counterTextView = findViewById(R.id.counterTextView)
+
+        //Actualizar o definir el valor del inicial de counterTextView -> counterTextView = score -> 0
+
+        //TODO en algun moment haurem d'executar incementCounter
+
+        tapMeButton.setOnClickListener{
+            incrementCounter()
+            //TODO iniciar el comptador
+        }
+        timeTextView.text=getString(R.string.timeText, time)
+    }
+
+    private fun incrementCounter(){
+
+        counter++
+        counterTextView.text = counter.toString()
+
     }
 }
